b9cb3414d92ec4f5596fdab0c812bfbb3ecfb920
Raghavesh <bosky2001@gmail.com>
2024-02-19 22:43:20 -0500

    mppi v1 impl

---
+++ kinematic_mpc.py
@@ -0,0 +1,110 @@
+# MIT License
+
+# Copyright (c) Hongrui Zheng, Johannes Betz
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+STMPC waypoint tracker example
+
+Author: Hongrui Zheng
+Last Modified: 8/1/22
+"""
+
+import numpy as np
+import gymnasium as gym
+from f110_gym.envs import F110Env
+import time
+from mppi import MPPIPlanner
+from f1tenth_planning.control.kinematic_mpc.kinematic_mpc import KMPCPlanner
+
+
+def main():
+    """
+    STMPC example. This example uses fixed waypoints throughout the 2 laps.
+    For an example using dynamic waypoints, see the lane switcher example.
+    """
+
+    # create environment
+    env: F110Env = gym.make(
+        "f110_gym:f110-v0",
+        config={
+            "map": "Spielberg",
+            "num_agents": 1,
+            "control_input": "accl",
+            "observation_config": {"type": "dynamic_state"},
+        },
+        render_mode="human",
+    )
+
+    # create planner
+    planner = KMPCPlanner(track=env.track, debug=False)
+    testplanner = MPPIPlanner(track=env.track)
+    planner.config.dlk = (
+        env.track.raceline.ss[1] - env.track.raceline.ss[0]
+    )  # waypoint spacing
+
+    testplanner.config.dlk = (
+        env.track.raceline.ss[1] - env.track.raceline.ss[0]
+    ) 
+    # env.unwrapped.add_render_callback(planner.render_waypoints)
+    # env.unwrapped.add_render_callback(planner.render_local_plan)
+
+    # env.unwrapped.add_render_callback(testplanner.render_waypoints)
+    # env.unwrapped.add_render_callback(testplanner.render_local_plan)
+    env.unwrapped.add_render_callback(testplanner.render_sampled_plan)
+    # env.unwrapped.add_render_callback(planner.render_mpc_sol)
+
+    # reset environment
+    poses = np.array(
+        [
+            [
+                env.track.raceline.xs[0],
+                env.track.raceline.ys[0],
+                env.track.raceline.yaws[0],
+            ]
+        ]
+    )
+    obs, info = env.reset(options={"poses": poses})
+    done = False
+    env.render()
+
+    laptime = 0.0
+    start = time.time()
+    while not done:
+        steerv, accl = planner.plan(obs["agent_0"])
+        _,_ = testplanner.plan(obs["agent_0"])
+        obs, timestep, terminated, truncated, infos = env.step(
+            np.array([[steerv, accl]])
+        )
+        done = terminated or truncated
+        laptime += timestep
+        env.render()
+
+        print(
+            "speed: {}, steer vel: {}, accl: {}".format(
+                obs["agent_0"]["linear_vel_x"], steerv, accl
+            )
+        )
+
+    print("Sim elapsed time:", laptime, "Real elapsed time:", time.time() - start)
+
+
+if __name__ == "__main__":
+    main()
